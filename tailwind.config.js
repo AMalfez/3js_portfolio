762213237d588eaba16bd24d18c60e85fd640679
AMalfez <a_mansuri@ch.iitr.ac.in>
2023-09-11 22:06:54 +0530

    now tailwind is working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ["./index.html","./src/**/*.{js,jsx}"],
   mode: "jit",
   theme: {
     extend: {
@@ -24,4 +24,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}; 
